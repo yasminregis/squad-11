2098d4aaf4c7b0153edfdef821993f00d70f2b3b
tiagojosedasilva <tiago.estudo.desenvolvimento@gmail.com>
2022-11-23 17:36:20 -0300

    add paginas com novo layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,41 @@
-var nomeDesafio;
-function enviar() {
-    nomeDesafio = document.getElementById("nome-desafio").value;
-    localStorage.info = document.getElementById("nome-representante").value;
-    localStorage.info = document.getElementById("nome-empresa").value;
-    localStorage.info = document.getElementById("email").value;
-    localStorage.info = document.getElementById("whatsapp").value;
-    localStorage.info = document.getElementById("discord").value;
-    localStorage.info = document.getElementById("representante-substituto").value;
-    localStorage.info = document.getElementById("duvidas").value;
-    localStorage.info = document.getElementById("descricao-problema").value;
-    localStorage.info = document.getElementById("descricao-solucao").value;
-    localStorage.info = document.getElementById("tecnologias-sugeridas").value;
-    /*localStorage.info = document.getElementById("materiais-referencia").value;*/
-    window.alert("Enviado com sucesso!");
+var firstname;
+var lastname;
+var email;
+var number;
+var discord;
+var nameSubst;
+var describeProblem;
+var describeSolution;
+var tecnolog;
+
+function enviar(){
+    firstname = document.getElementById('firstname').value;
+    lastname = document.getElementById('lastname').value;
+    email = document.getElementById('email').value;
+    number = document.getElementById('number').value;
+    discord = document.getElementById('discord').value;
+    nameSubst = document.getElementById('nameSubst').value;
+    describeProblem = document.getElementById('describeProblem').value;
+    describeSolution = document.getElementById('describeSolution').value;
+    tecnolog = document.getElementById('tecnolog').value;
+    localStorage.setItem('firstname', firstname);
+    localStorage.setItem('lastname', lastname);
+    localStorage.setItem('email', email);
+    localStorage.setItem('number', number);
+    localStorage.setItem('discord', discord);
+    localStorage.setItem('nameSubst', nameSubst);
+    localStorage.setItem('describeProblem', describeProblem);
+    localStorage.setItem('describeSolution', describeSolution);
+    localStorage.setItem('tecnolog', tecnolog);
 }
-function carregar() {
-    document.getElementById("nome-desafio").value = nomeDesafio;
-    document.getElementById("nome-representante").value = localStorage.info;
-    document.getElementById("nome-empresa").value = localStorage.info;
-    document.getElementById("email").value = localStorage.info;
-    document.getElementById("whatsapp").value = localStorage.info;
-    document.getElementById("discord").value = localStorage.info;
-    document.getElementById("representante-substituto").value = localStorage.info;
-    document.getElementById("duvidas").value = localStorage.info;
-    document.getElementById("descricao-problema").value = localStorage.info;
-    document.getElementById("descricao-solucao").value = localStorage.info;
-    document.getElementById("tecnologias-sugeridas").value = localStorage.info;
-    /*document.getElementById("materias-refencia").value = localStorage.info;*/
+function carregar(){
+    document.getElementById('firstname').value = localStorage.firstname; 
+    document.getElementById('lastname').value = localStorage.lastname; 
+    document.getElementById('email').value = localStorage.email; 
+    document.getElementById('number').value = localStorage.number; 
+    document.getElementById('discord').value = localStorage.discord;
+    document.getElementById('nameSubst').value = localStorage.nameSubst; 
+    document.getElementById('describeProblem').value = localStorage.describeProblem; 
+    document.getElementById('describeSolution').value = localStorage.describeSolution; 
+    document.getElementById('tecnolog').value = localStorage.tecnolog; 
 }
