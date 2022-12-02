aaab85f376916f635e6382cfa94c1f8f43931e23
tiagojosedasilva <tiago.estudo.desenvolvimento@gmail.com>
2022-12-02 16:40:15 -0300

    Concluindo algumas telas

--- scripts/Scriptlogin.js
+++ scripts/Scriptlogin.js
@@ -3,7 +3,7 @@ const navMenu = document.querySelector(".nav-menu");
 
 hamburguer.addEventListener("click",() => {
     hamburguer.classList.toggle('active');
-    navMenu. classList.toggle('active');
+    navMenu.classList.toggle('active');
 })
 
 
